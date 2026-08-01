abf4220d674b04fc75938d0da2d06f9168603d82
SUMANTH R <142378826+sumanthr2005@users.noreply.github.com>
2026-08-01 23:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (69.71%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[21][21];
+
+    int solve(vector<int>& nums, int i, int j) {
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != INT_MIN)
+            return dp[i][j];
+
+        int takeLeft = nums[i] - solve(nums, i + 1, j);
+        int takeRight = nums[j] - solve(nums, i, j - 1);
+
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        for (int i = 0; i < 21; i++)
+            for (int j = 0; j < 21; j++)
+                dp[i][j] = INT_MIN;
+
+        return solve(nums, 0, nums.size() - 1) >= 0;
+    }
+};
